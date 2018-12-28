1380d9fa53a122c3816c576660e71665a3e1b221
Alex Feldman-Crough <alex@fldcr.com>
2018-12-28 12:10:49 -0800

    Add info.rkt file

--- info.rkt
+++ info.rkt
@@ -1,6 +1,13 @@
 #lang info
 
-(define collection 'multi)
+(define collection
+  'multi)
+
+(define version
+  "0.1")
 
 (define deps
   '(("base" #:version "7.0")))
+
+(define build-deps
+  '("rackunit"))
